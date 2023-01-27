44bae3fa326ed9e26c4734239af01d3640dbe652
Yuliya <i@yzubkova.ru>
2023-01-27 12:41:16 +0300

    Задать двумерный массив, найти сумму элементов находящихся на главной диагонали

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 46: Задайте двумерный массив. 
+Найдите сумму элементов находящихся на главной диагонали
+с индексами (0,0); (1,1); (2,2)...
+
+
+1. Получить числа с консоли
+2. Задать матрицу и заполнить ее числами с консоли
+3. Распечатать матрицу
+
+*/
+
+
+// Получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено не число.");
+
+        }
+    }
+
+    return result;
+}
+
+//Задать матрицу и заполнить ее числами с консоли
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+//Распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Замена элементов массива
+void GetNewMatrix(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + matrix[i,j];
+            }
+        }
+    }
+    Console.WriteLine($"Cумма элементов, находящихся на главной диагонали равна {sum}");
+}
+
+int m = GetNumber("Введите количество строк: ");
+int n = GetNumber("Введите количество столбцов: ");
+
+int[,] matrix = GetMatrix(m, n);
+
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+GetNewMatrix(matrix);
+Console.WriteLine();
