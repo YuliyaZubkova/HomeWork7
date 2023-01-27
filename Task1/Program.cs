0cdb24f67f6f00d48984f2a985b03e1c3078173b
Yuliya <i@yzubkova.ru>
2023-01-27 10:55:04 +0300

    Задать двумерный массив, заполненный случайными целыми числами

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 46: Задайте двумерный массив размером m×n,
+ заполненный случайными целыми числами.
+ m = 3, n = 4.
+ 1 4 8 19
+ 5 -2 33 -2
+ 77 3 8 1
+
+1. Получить числа с консоли
+2. Задать матрицу и заполнить ее числами с консоли
+3. Распечатать матрицу
+
+*/
+
+
+// Получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено не число.");
+
+        }
+    }
+
+    return result;
+}
+
+//Задать матрицу и заполнить ее числами с консоли
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+//Распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите количество строк: ");
+int n = GetNumber("Введите количество столбцов: ");
+
+int[,] matrix = GetMatrix(m, n);
+
+PrintMatrix(matrix);
